f8c4bcde50a232441d80c2617f330773ae70fe20
Wellington Andrade <wmazoni@protonmail.com>
2020-03-04 20:05:28 -0300

    Manipulando pastas com File

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,22 +1,32 @@
 package application;
 
-import java.io.BufferedWriter;
-import java.io.FileWriter;
-import java.io.IOException;
+import java.io.File;
+import java.util.Scanner;
 
 public class Program {
 
 	public static void main(String[] args) {
-		String[] lines = new String[] {"Good morning", "Good afternoon", "Good night"};
-		String path = "C:\\Users\\wmazo\\Documents\\repositorios\\java\\ws-eclipse\\out.txt";
-		try (BufferedWriter bw = new BufferedWriter(new FileWriter(path, true))){
-			for (String line : lines) {
-				bw.write(line);
-				bw.newLine();
-			}
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter a folder path: ");
+		String strPath = sc.nextLine();
+		
+		File path = new File(strPath);
+		
+		File[] folders = path.listFiles(File::isDirectory);
+		System.out.println("FOLDERS:");
+		for (File folder : folders) {
+			System.out.println(folder);
 		}
-		catch (IOException e) {
-			e.printStackTrace();
+		
+		File[] files = path.listFiles(File::isFile);
+		System.out.println("FILES:");
+		for (File file : files) {
+			System.out.println(file);
 		}
+		
+		boolean success = new File(strPath + "\\subdir").mkdir();
+		System.out.println("Directory created successfully: " + success);
+		
+		sc.close();
 	}
 }
