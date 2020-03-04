1f8c1636fde4c3f77681fead0e33f6d4bcfae267
Wellington Andrade <wmazoni@protonmail.com>
2020-03-04 19:38:12 -0300

    FileReader e BufferedReader

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,28 +1,38 @@
 package application;
 
-import java.io.File;
+import java.io.BufferedReader;
+import java.io.FileReader;
 import java.io.IOException;
-import java.util.Scanner;
 
 public class Program {
 
 	public static void main(String[] args) {
-		File file = new File("C:\\Users\\wmazo\\Documents\\repositorios\\java\\ws-eclipse\\in.txt");
-		Scanner sc = null;
+		String path = "C:\\Users\\wmazo\\Documents\\repositorios\\java\\ws-eclipse\\in.txt";
+		FileReader fr = null;
+		BufferedReader br = null;
+
 		try {
-			sc = new Scanner(file);
-			while (sc.hasNextLine()) {
-				System.out.println(sc.nextLine());
+			fr = new FileReader(path);
+			br = new BufferedReader(fr);
+			String line = br.readLine();
+			while (line != null) {
+				System.out.println(line);
+				line = br.readLine();
 			}
-		}
-		catch (IOException e) {
+		} catch (IOException e) {
 			System.out.println("Error: " + e.getMessage());
-		}
-		finally {
-			if (sc != null) {
-				sc.close();
+		} finally {
+			try {
+				if (br != null) {
+					br.close();
+				}
+				if (fr != null) {
+					fr.close();
+				}
+			} catch (IOException e) {
+				e.printStackTrace();
 			}
+
 		}
 	}
-
 }
