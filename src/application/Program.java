c62b003908fd24d6d1edd648e1231bf63cb63723
Wellington Andrade <wmazoni@protonmail.com>
2020-03-04 19:53:07 -0300

    FileWriter e BufferedWriter

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,22 +1,22 @@
 package application;
 
-import java.io.BufferedReader;
-import java.io.FileReader;
+import java.io.BufferedWriter;
+import java.io.FileWriter;
 import java.io.IOException;
 
 public class Program {
 
 	public static void main(String[] args) {
-		String path = "C:\\Users\\wmazo\\Documents\\repositorios\\java\\ws-eclipse\\in.txt";
-
-		try (BufferedReader br = new BufferedReader(new FileReader(path))) {
-			String line = br.readLine();
-			while (line != null) {
-				System.out.println(line);
-				line = br.readLine();
+		String[] lines = new String[] {"Good morning", "Good afternoon", "Good night"};
+		String path = "C:\\Users\\wmazo\\Documents\\repositorios\\java\\ws-eclipse\\out.txt";
+		try (BufferedWriter bw = new BufferedWriter(new FileWriter(path, true))){
+			for (String line : lines) {
+				bw.write(line);
+				bw.newLine();
 			}
-		} catch (IOException e) {
-			System.out.println("Error: " + e.getMessage());
+		}
+		catch (IOException e) {
+			e.printStackTrace();
 		}
 	}
 }
