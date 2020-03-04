c834f45206e33974e296fb0c9568bf8447ffc63c
Wellington Andrade <wmazoni@protonmail.com>
2020-03-04 19:42:58 -0300

    Bloco try-with-resources

--- src/application/Program.java
+++ src/application/Program.java
@@ -8,12 +8,8 @@ public class Program {
 
 	public static void main(String[] args) {
 		String path = "C:\\Users\\wmazo\\Documents\\repositorios\\java\\ws-eclipse\\in.txt";
-		FileReader fr = null;
-		BufferedReader br = null;
 
-		try {
-			fr = new FileReader(path);
-			br = new BufferedReader(fr);
+		try (BufferedReader br = new BufferedReader(new FileReader(path))) {
 			String line = br.readLine();
 			while (line != null) {
 				System.out.println(line);
@@ -21,18 +17,6 @@ public class Program {
 			}
 		} catch (IOException e) {
 			System.out.println("Error: " + e.getMessage());
-		} finally {
-			try {
-				if (br != null) {
-					br.close();
-				}
-				if (fr != null) {
-					fr.close();
-				}
-			} catch (IOException e) {
-				e.printStackTrace();
-			}
-
 		}
 	}
 }
